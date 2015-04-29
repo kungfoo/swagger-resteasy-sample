28b99c774fbe597d8970ecbc66f99bbe550a74b7
Silvio Heuberger <silvio.heuberger@ergon.ch>
2015-04-29 15:13:42 +0200

    Add a @BeanParam query class and return stub data accordingly

--- resteasy-sample/src/main/java/ch/ergon/sample/rest/resources/PetQuery.java
+++ resteasy-sample/src/main/java/ch/ergon/sample/rest/resources/PetQuery.java
@@ -1,5 +1,87 @@
 package ch.ergon.sample.rest.resources;
 
-public class PetQuery {
+import static com.google.common.base.Preconditions.checkNotNull;
 
+import javax.ws.rs.QueryParam;
+
+import ch.ergon.sample.rest.resources.Pet.Kind;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+public class PetQuery {
+	@QueryParam("maxAge")
+	private Integer maxAgeInYears;
+
+	@QueryParam("kind")
+	private Kind kind;
+
+	public Integer getMaxAgeInYears() {
+		return maxAgeInYears;
+	}
+
+	public void setMaxAgeInYears(Integer maxAgeInYears) {
+		this.maxAgeInYears = maxAgeInYears;
+	}
+
+	public Kind getKind() {
+		return kind;
+	}
+
+	public void setKind(Kind kind) {
+		this.kind = kind;
+	}
+
+	public Predicate<Pet> toPredicate() {
+		return new PetPredicate(this);
+	}
+
+	public static class PetPredicate implements Predicate<Pet> {
+
+		private final PetQuery query;
+
+		public PetPredicate(PetQuery query) {
+			this.query = checkNotNull(query);
+		}
+
+		@Override
+		public boolean apply(Pet aPet) {
+			return Predicates.and(agePredicate(), kindPredicate()).apply(aPet);
+		}
+
+		private Predicate<Pet> kindPredicate() {
+			if (query.kind == null) {
+				return Predicates.alwaysTrue();
+			}
+			return new Predicate<Pet>() {
+				@Override
+				public boolean apply(Pet pet) {
+					return pet.getKind() == query.kind;
+				}
+			};
+		}
+
+		private Predicate<Pet> agePredicate() {
+			if (query.maxAgeInYears == null) {
+				return Predicates.alwaysTrue();
+			}
+			return new Predicate<Pet>() {
+				@Override
+				public boolean apply(Pet pet) {
+					return pet.getAgeInYears() <= query.maxAgeInYears;
+				}
+			};
+		}
+
+		@Override
+		public String toString() {
+			return MoreObjects.toStringHelper(this)
+					.add("maxAge", query.maxAgeInYears)
+					.add("kind", query.kind)
+					.omitNullValues()
+					.toString();
+		}
+
+	}
 }
--- resteasy-sample/src/main/java/ch/ergon/sample/rest/resources/PetResource.java
+++ resteasy-sample/src/main/java/ch/ergon/sample/rest/resources/PetResource.java
@@ -4,7 +4,9 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,18 +14,30 @@ import javax.ws.rs.Produces;
 
 import ch.ergon.sample.rest.resources.Pet.Kind;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+
 @Path("/pets")
 @Produces("application/json")
 @Consumes("application/json")
 @LogExecutionTime
 public class PetResource {
 	
+	
+	private static Logger LOGGER = Logger.getLogger(PetResource.class.getSimpleName());
+	private List<Pet> pets = createPets();
+	
 	@GET
     @Path("/")
-	public PetListResult query(PetQuery query) {
+	public PetListResult query(@BeanParam PetQuery query) {
+		Predicate<Pet> predicate = query.toPredicate();
+		LOGGER.info(String.format("Filtering with %s", predicate));
+		ImmutableList<Pet> petsMatchingQuery = FluentIterable.from(pets).filter(predicate).toList();
+		
 		PetListResult result = new PetListResult();
 		result.setNotifications(createNotifications());
-		result.setPets(createPets());
+		result.setPets(petsMatchingQuery);
 		return result;
 	}
 
